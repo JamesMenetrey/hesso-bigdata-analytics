2e64b848d68c5519a04ed8301c2d7249b610a668
Jämes Ménétrey <james@menetrey.me>
2019-06-05 21:19:32 +0200

    Add the GeoJSON parser.

---
+++ src/geojson-parser/GeoJsonParser/Models/FeatureModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace GeoJsonParser.Models
+{
+    public class FeatureModel
+    {
+        public FeatureModel()
+        {
+            Geometry = new GeometryModel();
+        }
+
+        [JsonProperty(PropertyName = "type")] public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "geometry")]
+        public GeometryModel Geometry { get; set; }
+    }
+}
